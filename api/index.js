3cffb1d9fd66be6490e01226ae499958028ead42
kallamlakshmidhar0084 <149854537+kallamlakshmidhar0084@users.noreply.github.com>
2024-03-08 16:46:02 +0530

    Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
-
 const express=require('express');
+const mongoose=require('mongoose');
 const app=express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to mongoDB");
+}).catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3000 , () =>{
-    console.log("server running on server 3000");
+    try {
+        console.log("server running on server 3000");
+
+    } catch (error) {
+        console.log(error);
+    }
 })
